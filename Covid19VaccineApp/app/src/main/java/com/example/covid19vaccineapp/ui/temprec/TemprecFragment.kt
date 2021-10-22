23d07176163e7e5bf32023153266e8f143356742
Docat0209 <shane930209@gmail.com>
2021-10-22 15:58:28 +0800

    update

--- Covid19VaccineApp/app/src/main/java/com/example/covid19vaccineapp/ui/temprec/TemprecFragment.kt
+++ Covid19VaccineApp/app/src/main/java/com/example/covid19vaccineapp/ui/temprec/TemprecFragment.kt
@@ -1,11 +1,20 @@
 package com.example.covid19vaccineapp.ui.temprec
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.covid19vaccineapp.R
 import com.example.covid19vaccineapp.databinding.FragmentTemprecBinding
+import android.app.AlertDialog.Builder
+import android.graphics.Canvas
+import android.widget.SeekBar
+import android.widget.TextView
+
 
 class TemprecFragment : Fragment() {
 
@@ -23,6 +32,40 @@ class TemprecFragment : Fragment() {
 
         _binding = FragmentTemprecBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        // tool bar
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarTemprec.toolbar)
+        val toolbar = (activity as AppCompatActivity).supportActionBar
+        toolbar!!.setDisplayShowTitleEnabled(false)
+        binding.toolbarTemprec.toolbarTitle.text = resources.getString(R.string.title_temprec)
+        binding.toolbarTemprec.toolbarCancel.setOnClickListener{
+            Navigation.findNavController(it).popBackStack()
+        }
+        binding.toolbarTemprec.toolbarAdd.setOnClickListener {
+            val dialogBuilder = Builder(context)
+            val inflater = this.layoutInflater
+            val dialogView = inflater.inflate(R.layout.alert_label_editor, null)
+            dialogBuilder.setView(dialogView)
+            val alertDialog = dialogBuilder.create()
+            alertDialog.show()
+            alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+            val seekBar = dialogView.findViewById<SeekBar>(R.id.seek_progress)
+            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
+                    val textView = dialogView.findViewById<TextView>(R.id.bar)
+                    textView.text = "$progress°c"
+                    textView.x = seekBar.x
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+                }
+
+            })
+        }
 
 
         return root
@@ -32,4 +75,7 @@ class TemprecFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
+
 }
